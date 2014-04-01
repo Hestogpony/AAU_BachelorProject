3bc0af421a61b286b21da121cb77a792cdd4c185
Mikkel Alexander Madsen <mikkelarm@gmail.com>
2014-04-01 15:24:18 +0200

    fixed graphparser and loader

--- code/graph/graphload.py
+++ code/graph/graphload.py
@@ -4,13 +4,14 @@ import re
 import networkx as nx
 from operator import itemgetter
 from haversine import distance
+from time import sleep
 
 class OSMLoader():
-    def __init__(self,xmin,ymin,xmax,ymax):
-        self.xmin = xmin
-        self.ymin = ymin
-        self.xmax = xmax
-        self.ymax = ymax
+    def __init__(self,lonmin,latmin,lonmax,latmax):
+        self.lonmin = lonmin
+        self.lonmax = lonmax
+        self.latmin = latmin
+        self.latmax = latmax
         self.conn = self.connect()
         self.cur = self.conn.cursor()
         self.intersection = []
@@ -19,8 +20,8 @@ class OSMLoader():
         return psycopg2.connect(database="osmgraph",port='5432', host='127.0.0.1', user="mikkel", password="syrlinger")
 
     def toGraph(self):
-        self.graph = nx.DiGraph()
-        self.cur.execute('select n1.id, n1.lat, n1.lon, n2.id, n2.lat, n2.lon, name  from nodes as n1, nodes as n2, edges where n1.id=node1 and n2.id=node2 and n1.lon between {0} and {1} and n2.lon between {0} and {1} and n1.lat between {2} and {3} and n2.lat between {2} and {3};'.format(self.xmin, self.xmax, self.ymin, self.ymax))
+        self.graph = nx.Graph()
+        self.cur.execute('select n1.id, n1.lat, n1.lon, n2.id, n2.lat, n2.lon, name  from nodes as n1, nodes as n2, edges where n1.id=node1 and n2.id=node2 and n1.lon between {0} and {1} and n2.lon between {0} and {1} and n1.lat between {2} and {3} and n2.lat between {2} and {3};'.format(self.lonmin, self.lonmax, self.latmin, self.latmax))
         nexttuple = self.cur.fetchone()
 
         while nexttuple is not None:
@@ -28,25 +29,32 @@ class OSMLoader():
             self.graph.add_edge(nexttuple[0],nexttuple[3],weight=dist, name=nexttuple[6])
             nexttuple = self.cur.fetchone()
 
+    def roadname_from_id(self, id):
+        pass
+
 def findpaths(G,s,d):
     paths = []
     for node in G.nodes():
-        toNode = nx.shortest_path(G,s,node)
-        fromNode = nx.shortest_path(G,node,d)
-        paths.append(toNode.extend(fromNode))
+        try:
+            toNode = nx.shortest_path(G,s,node)
+            fromNode = nx.shortest_path(G,node,d)
+            paths.append(toNode.extend(fromNode))
+        except:
+            osmloader.graph.remove_node(node)
     return paths
 
 
-osmloader = OSMLoader(12.33619, 55.67397, 12.56292, 55.74944)
+osmloader = OSMLoader(12.422791,55.571746,12.702255,55.742187)
 osmloader.toGraph()
-print osmloader.graph.nodes()
-import pylab
-nx.draw(osmloader.graph) 
-pylab.show()
+G=osmloader.graph
+
+print len(nx.all_simple_paths(G,331138613,92920533))
+
+# bfs =  nx.bfs_tree(G,1736564314)  8086156L,2190257445L
 
 
 
-# abe = nx.shortest_path(osmloader.graph, 118792, 1589573142, weight='weight')
-# print findpaths(osmloader.graph, 118792, 1589573142)
 
-
+# print osmloader.graph.nodes()
+#for e in osmloader.graph.edges():
+#    print e, osmloader.graph[e[0]][e[1]]['name']
