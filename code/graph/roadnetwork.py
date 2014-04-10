263f8acbdf3c972ec92a0cf729a71b57508db713
Hestogpony <simonbuusjensen@hotmail.com>
2014-04-10 15:16:00 +0200

    rn_algorithms.py tilføjet

--- code/graph/roadnetwork.py
+++ code/graph/roadnetwork.py
@@ -1,30 +1,26 @@
 
 import networkx as nw
 
-class Road_network(nw.Graph): 
+class RoadNetwork(nw.Graph): 
     
-    def __init__(self, node, params):
-        super(Road_network, self).__init__()
+    def __init__(self, params):
+        super(RoadNetwork, self).__init__()
     
     def generate_charge(self):
         pass
     
-    def max_velocity(self):
-        pass
-    
-    def fastest_path_optimal(self):
-        pass
-    
-    def fastest_path_naive(self):
-        pass
     
     def charge_rate(self, node):
         pass
     
-
-g = Road_network(2, 3)
+    def prune(self):
+        pass
+    
+    
+    
+    
+g = RoadNetwork(2, 3)
 
 g.add_node(41)
 
-print(g.nodes())
 
