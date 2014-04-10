4509b172c615352992d3751ab62ce6840579f775
Hestogpony <simonbuusjensen@hotmail.com>
2014-04-10 14:22:08 +0200

    ryddet op i kode og tilføjet roadnetwork.py

--- code/fast_path/bruteforce.py
+++ code/fast_path/bruteforce.py
@@ -3,7 +3,7 @@
 from networkx import all_simple_paths
 from path_utils import optimal_path_traversal
 def fastest_path(G, s, t, v):
-	"""returns the fastest path in directed graph G, from s to t, given a vehicle"""
+	"""returns the fastest path in directed roadnetwork G, from s to t, given a vehicle"""
 	path_generator = all_simple_paths(G, s, t)
 	path_times = []
 	for P in path_generator:
