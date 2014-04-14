63169f46ee15b7b5dd541b36567f6a4503477b62
Hestogpony <simonbuusjensen@hotmail.com>
2014-04-14 12:52:30 +0200

    stuff done

--- code/fastest_path/main.py
+++ code/fastest_path/main.py
@@ -3,7 +3,7 @@ import networkx as nx
 import os
 
 loader = loader.Loader()   
-loader.create_graph(12.422791,55.571746,12.702255,55.742187)
+loader.create_graph(6.02,53.7,14.99,57.96)
 loader.load_graph()
 G=loader.graph
 p = nx.shortest_path(G,loader.street_node('Elmegade'), loader.street_node('Kristianiagade'))
