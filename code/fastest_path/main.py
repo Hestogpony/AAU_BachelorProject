1a48a44bd118dfbb36d669de0697e5e5c4d1a2e6
Datdrol <andreas.b.eriksen@gmail.com>
2014-04-15 11:44:28 +0200

    Added a test BFS

--- code/fastest_path/main.py
+++ code/fastest_path/main.py
@@ -4,7 +4,6 @@ import networkx as nx
 import os
 import time
 
-<<<<<<< HEAD
 def main():
     
     loader = loader.Loader()   
@@ -16,6 +15,31 @@ def main():
     for x in xrange(1,10):
         print p[x]
     print len(p)
+
+def fastest_path(G):
+	pass
+
+def timecalculator(edges=[]):
+	pass
+
+
+def BFS(G, s):
+	loader = loader.Loader()
+    	loader.create_graph(9.972153,57.007566,10.005369,57.020594) ##aalborg øst
+    	loader.load_graph()
+    	G=loader.graph
+	return nx.bfs_tree(G, s)
+
+loader = loader.Loader()
+loader.create_graph(9.972153,57.007566,10.005369,57.020594) ##aalborg øst
+loader.load_graph()
+G=loader.graph
+s = loader.street_node('Selma Lagerløfs Vej')
+print BFS(G, s)
+def CR(v):
+	return v*v+v 
+
+
 
 def shortest_path_through_all(G,s,t):
         shortest_paths = []
