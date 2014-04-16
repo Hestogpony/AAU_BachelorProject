57a757a54bb168724c86eb3b00aa2984bd2cf33e
Hestogpony <simonbuusjensen@hotmail.com>
2014-04-16 14:53:36 +0200

    updateret moduler

--- code/fastest_path/main.py
+++ code/fastest_path/main.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
-#===============================================================================
-# import os
-# import time
-#===============================================================================
-
+import os
+import time
+import networkx as nx
 from loader import Loader
 import vehicle
 
@@ -11,60 +9,55 @@ import vehicle
 
 def main():
     
-    EV = vehicle.ElectricalVehicle(80)
     loader = Loader()
     loader.create_graph(9.667917,56.928277,10.205032,57.168636)  
     loader.load_graph()
     road_network = loader.rn
     road_network.generate_charge(10, 20)
-    stations = road_network.neighbors(2499936207)
-    for station in stations:
-        road_network.charge_rate(station)
-    
-    #print(road_network[2499936200][2499936211]['speed_limit'])
     road_network.visualize_roadnetwork()
     
-main()
+    #===========================================================================
+    # print len(G.nodes()), len(G.edges())
+    # p = shortest_path_through_all(G,loader.street_node('Selma LagerlÃ¸fs Vej'), loader.street_node('Niels Bohrs Vej'))
+    # for x in xrange(1,10):
+    #     print p[x]
+    # print len(p)
+    #===========================================================================
     
-#     print len(G.nodes()), len(G.edges())
-#     p = shortest_path_through_all(G,loader.street_node('Selma LagerlÃ¸fs Vej'), loader.street_node('Niels Bohrs Vej'))
-#     for x in xrange(1,10):
-#         print p[x]
-#     print len(p)
-#     
-#     
-# def shortest_path_through_all(G,s,t):
-#         shortest_paths = []
-#         for node in G.nodes():
-#             if node != s and node != t:
-#                 try:
-#                     p = nx.shortest_path(G,s,node) + nx.shortest_path(G,node,t)
-#                     shortest_paths.append(p)
-#                 except:
-#                     pass
-#         return shortest_paths
-=======
+main()
+     
+def shortest_path_through_all(G,s,t):
+        shortest_paths = []
+        for node in G.nodes():
+            if node != s and node != t:
+                try:
+                    p = nx.shortest_path(G,s,node) + nx.shortest_path(G,node,t)
+                    shortest_paths.append(p)
+                except:
+                    pass
+        return shortest_paths
+
 def fastest_path(G):
 	pass
-
+ 
 def timecalculator(edges=[]):
 	pass
-
-
+ 
+ 
 def BFS(G, s):
-	loader = loader.Loader()
-    	loader.create_graph(9.972153,57.007566,10.005369,57.020594) ##aalborg øst
-    	loader.load_graph()
-    	G=loader.graph
-	return nx.bfs_tree(G, s)
-
-loader = loader.Loader()
+    loader = loader.Loader()
+    loader.create_graph(9.972153,57.007566,10.005369,57.020594) ##aalborg øst
+    loader.load_graph()
+    G=loader.graph
+    return nx.bfs_tree(G, s)
+ 
+loader = Loader()
 loader.create_graph(9.972153,57.007566,10.005369,57.020594) ##aalborg øst
 loader.load_graph()
 G=loader.graph
 s = loader.street_node('Selma Lagerløfs Vej')
 print BFS(G, s)
-
+ 
 def CR(v):
 	return v*v+v 
 
--- code/fastest_path/roadnetwork.py
+++ code/fastest_path/roadnetwork.py
@@ -20,16 +20,13 @@ class RoadNetwork(nw.Graph):
                 self.node[node_id]['charge_rate'] = random_charge_rate
             else:
                 self.node[node_id]['charge_rate'] = 0
-            counter += 1    
-            #print(str(node_id) + " gets: " + str(random_charge_rate))
-    
+            counter += 1
                     
     def charge_rate(self, node_id):
-        print(str(self.node[node_id]['charge_rate']))
+        return self.node[node_id]['charge_rate']
         
     def visualize_roadnetwork(self):
         json_rn = (json.dumps([{'title': str(node),'lat':self.node[node]['lat'], 'lng':self.node[node]['lon'], 'charge_rate':self.node[node]['charge_rate']} for node in self.nodes()]))
-        print(json_rn)
         f = open('path.html','w')
         asdf = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><title></title></head><body><script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=false"></script><script type="text/javascript">var markers =%s; window.onload = function () {var mapOptions = {center: new google.maps.LatLng(markers[0].lat, markers[0].lng),zoom: 10,mapTypeId: google.maps.MapTypeId.ROADMAP};var map = new google.maps.Map(document.getElementById("dvMap"), mapOptions);var infoWindow = new google.maps.InfoWindow();var lat_lng = new Array();var latlngbounds = new google.maps.LatLngBounds();for (i = 0; i < markers.length; i++) {var data = markers[i];if (data.charge_rate > 0) {var myLatlng = new google.maps.LatLng(data.lat, data.lng);lat_lng.push(myLatlng);var marker = new google.maps.Marker({position: myLatlng,map: map,title: data.title,visible: true});latlngbounds.extend(marker.position);(function (marker, data) {google.maps.event.addListener(marker, "click", function (e) {infoWindow.setContent(data.description);infoWindow.open(map, marker);});})(marker, data);}}map.setCenter(latlngbounds.getCenter());map.fitBounds(latlngbounds);var path = new google.maps.MVCArray();var service = new google.maps.DirectionsService();var poly = new google.maps.Polyline({ map: map, strokeColor: "#4986E7" });/*for (var i = 0; i < lat_lng.length; i++) {if ((i + 1) < lat_lng.length) {var src = lat_lng[i];var des = lat_lng[i + 1];path.push(src);poly.setPath(path);}}*/}</script><div id="dvMap" style="width: 1000px; height: 600px"></div></body></html>' % (json_rn)
         f.write(asdf)
