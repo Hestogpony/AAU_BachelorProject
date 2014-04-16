fd3502cc1075645cec266d34eaea8bf61468898b
Mikkel Alexander Madsen <mikkelarm@gmail.com>
2014-04-16 15:50:21 +0200

    Yes

--- code/fastest_path/vehicle.py
+++ code/fastest_path/vehicle.py
@@ -1,12 +1,10 @@
 
-import math
 
 class ElectricalVehicle(object):
-   
     def __init__(self, battery_capacity):
         self.battery_capacity = battery_capacity # capacity in kWh
         
     # consumption rate in kWh/mile
-    def consumption_rate(self, speed, distance): 
-        return ((0.0286 * math.pow(speed, 2) + 0.4096 * speed + 107.57) * math.pow(10, -3)) * distance
+    def consumption_rate(self, speed): 
+        return (0.0286 * math.pow(speed, 2) + 0.4096 * speed + 107.57) * 10**(-3)
         
