a5ee03eaed04c17baa990658b00577447d044845
Mathias A <mrmathiasma@gmail.com>
2014-04-25 15:01:26 +0200

    slap auf mikkel

--- code/fastest_path/mathiasmain.py
+++ code/fastest_path/mathiasmain.py
@@ -2,23 +2,26 @@
 import networkx as nx
 import vehicle
 import naive
-import road_network
+import roadnetwork
 from loader import Loader
 
 loader = Loader()
-loader.create_graph(8.009,56.0843,11.1182,57.7979) # 7.43,54.47,12.94,57.74 <- danmark nord+midt -> 8.009,56.0843,11.1182,57.7979  
+loader.create_graph(7.43,54.47,12.94,57.74) # 7.43,54.47,12.94,57.74 <- danmark nord+midt -> 8.009,56.0843,11.1182,57.7979  
 loader.load_graph()
-road_network = loader.rn
-road_network.generate_charge(10,20)
-v = vehicle.ElectricalVehicle(80, 80)
+loader.remove_unconnected()
 
-print 'removing non-connected nodes'
-G = nx.connected_components(road_network)
-for node in road_network.nodes():
-    if node not in G[0]:
-	    road_network.remove_node(node
 
-print 'graph loaded'
-#bfs = nx.bfs_edges(road_network,loader.street_node('Selma Lagerløfs Vej'))
-path = naive.naive_path(road_network, v, loader.street_node('Brettevillesgade'), loader.street_node('Bethaniagade'))
-print path
+# roadnetwork = loader.rn
+# roadnetwork.generate_charge(10,20)
+# v = vehicle.ElectricalVehicle(80, 80)
+
+# print 'removing non-connected nodes'
+# G = nx.connected_components(roadnetwork)
+# for node in roadnetwork.nodes():
+#     if node not in G[0]:
+# 	    roadnetwork.remove_node(node)
+
+# print 'graph loaded'
+# #bfs = nx.bfs_edges(roadnetwork,loader.street_node('Selma Lagerløfs Vej'))
+# path = naive.naive_path(roadnetwork, v, loader.street_node('Brettevillesgade'), loader.street_node('Bethaniagade'))
+# print path
