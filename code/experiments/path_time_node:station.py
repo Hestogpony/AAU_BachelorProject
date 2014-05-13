cb14c5994905420c655822a6fa4e533d33e7d37a
Mathias A <mrmathiasma@gmail.com>
2014-05-13 10:25:20 +0200

    struktur

--- code/experiments/path_time_node:station.py
+++ code/experiments/path_time_node:station.py
@@ -3,3 +3,15 @@ Bounding box Denmark
 Produce at least one S to one T 
 Remove 0 to 100 procent of charge stations
 """
+import importer
+import networkx as nx
+import fastest_path.roadnetwork
+from fastest_path.haversine import distance
+from fastest_path.roadnetwork import RoadNetwork
+
+rn = RoadNetwork(nx.read_gpickle('pickle_experiment'))
+
+
+def charge_station_density(rn, dist):
+	for node in rn.nodes():
+		print nx.single_source_dijkstra(rn,node,cutoff=dist,weight='weight')
