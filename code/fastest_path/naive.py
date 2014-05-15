f29db35f39de0f69d89d2260057a25e6a41d757d
Mikkel Alexander Madsen <mikkelarm@gmail.com>
2014-05-15 14:15:46 +0200

    more methods for visualise and new naive

--- code/fastest_path/naive.py
+++ code/fastest_path/naive.py
@@ -14,7 +14,7 @@ def naive_path(rn, v, s, t):
     while cur_node != t:
         shortest_through_path = []
         shortest_through = float('inf')
-        look_at= []
+        d,sp = nx.single_source_dijkstra(rn, cur_node, cutoff=car_range)
         for node in rn.nodes(): #find all charge stations within car_range as the crows flies
             if node == t:
                 sp_t = nx.shortest_path(rn, cur_node, t, weight='weight')
@@ -23,29 +23,25 @@ def naive_path(rn, v, s, t):
                     break
             elif rn.node[node]['charge_rate'] != 0 and\
             distance((float(rn.node[node]['lat']),float(rn.node[node]['lon'])),
-                     (float(rn.node[cur_node]['lat']),float(rn.node[cur_node]['lon']))) <= car_range and node != cur_node:
-                look_at.append(node)
-                try:
-                    if (shortest_through != float('inf')):
-                        sp_char = nx.shortest_path(rn, cur_node, node, weight='weight', cutoff=shortest_through)
-                    else:
-                        sp_char = nx.shortest_path(rn, cur_node, node, weight='weight')
-                except:
+                     (float(rn.node[cur_node]['lat']),float(rn.node[cur_node]['lon']))) <= car_range and cur_node != node:
+                if node in sp:
+                    sp_char = sp[node]
+                else:
                     continue
                 if reachable(rn, sp_char, v, bat):
-                    sp_t = nx.shortest_path(rn, node, t, weight='weight')
-                    del sp_t[0]
-                    combine_path = sp_char + sp_t
-                    if shortest_through > path_length(rn, sp_t):
-                        shortest_through = path_length(rn, combine_path)
+                    # sp_t = nx.shortest_path(rn, node, t, weight='weight')
+                    # del sp_t[0]
+                    haversine_t = distance((float(rn.node[node]['lat']),float(rn.node[node]['lon'])),(float(rn.node[t]['lat']),float(rn.node[t]['lon'])))
+                    if shortest_through > haversine_t:
+                        shortest_through = haversine_t
                         shortest_through_path = sp_char
-        rn.visualize_nodes(look_at[:500])
         if shortest_through_path:
             route_plan_time += path_time(rn, shortest_through_path) + charge_time(rn, shortest_through_path, v) #add time spend driven and charging for this sub-path
             print 'cur: ',cur_node, 'next:',shortest_through_path[-1]
             cur_node = shortest_through_path[-1]
             del shortest_through_path[0]
             driven_path += shortest_through_path
+            rn.visualize_nodes([s,cur_node,t])
         else:
             return [],float('inf')
     return driven_path, route_plan_time
