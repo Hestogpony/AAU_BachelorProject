2fcff1b69c158f9c330abbc34f320c8d7df390d1
Mathias A <mrmathiasma@gmail.com>
2014-05-15 13:21:50 +0200

    flere eksperimenter

--- code/experiments/importer.py
+++ code/experiments/importer.py
@@ -1,8 +1,3 @@
 import os, sys
 lib_path = os.path.abspath(__file__ +'/../../')
 sys.path.append(lib_path)
-import networkx as nx
-import fastest_path.roadnetwork
-from fastest_path.haversine import distance
-from fastest_path.roadnetwork import RoadNetwork
-import time
