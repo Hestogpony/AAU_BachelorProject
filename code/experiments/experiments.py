217412cc134dc276fb271dfd41cddfd6ef7d2099
Mathias A <mrmathiasma@gmail.com>
2014-05-16 11:19:45 +0200

    lel

--- code/experiments/experiments.py
+++ code/experiments/experiments.py
@@ -184,26 +184,40 @@ def experiment_driving_dist(ev, CS_density, max_distance):
 	f = open('path_time_length(1-500).csv', 'a')
 	f.write('dist,time,fail_rate\n')
 	for distance in range(1, max_distance, 50):
-		sum_time, fails = 0,0
+		naive_sum_time, greedy_sum_time, lp_sum_time, naive_fails, greedy_fails, lp_fails = 0,0,0,0,0,0
 		for x in xrange(0,10):
 			s,t,dist = s_and_t(rn, distance)
 			
 			################ NAIVE
 			naive_p, naive_t = naive_path(rn, ev, s, t)
 			if naive_t == float('inf'):
-				fails += 1
+				naive_fails += 1
 			else:
-				sum_time += naive_t
+				naive_sum_time += naive_t
 				
 			################ GREEDY
-			
+
+			if greedy_return == float('inf'):
+				greedy_fails += 1
+			else:
+				greedy_sum_time += greedy_return
 
 			################ LP
 
+			if lp_return == float('inf'):
+				lp_fails += 1
+			else:
+				lp_sum_time += lp_return
 
 			print 'From S:%s to T:%s - Distance:%sKm +-%s - %shours'%(s,t,distance,abs(distance-dist),naive_t)
 
-		f.write('%s,%s,%s\n' % (distance, sum_time/10.0, fails/10.0))
+		f.write('%s,%s,%s,%s,%s,%s,%s\n' % (distance, 
+											naive_sum_time/10.0,
+											naive_fails/10.0, 
+											greedy_sum_time/10.0,
+											greedy_fails/10.0, 
+											lp_sum_time/10.0, 
+											lp_fails/10.0))
 	f.close()
 
 road_network = RoadNetwork(nx.read_gpickle('pickle_experiment'))
